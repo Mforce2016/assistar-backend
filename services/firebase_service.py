121e9bd119558daeb0b6ff888591bfc8fc60db40
Javier <ginouman.jb@gmail.com>
2026-05-09 12:54:48 -0300

    Actualizacion firebase_service

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,20 +1,43 @@
 import firebase_admin
+import json
+import os
 
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 
+# =====================================
+# INICIAR FIREBASE
+# =====================================
+
 def iniciar_firebase():
+
     try:
         firebase_admin.get_app()
 
     except:
+
+        firebase_json = os.getenv(
+            "FIREBASE_CREDENTIALS"
+        )
+
+        if not firebase_json:
+            raise Exception(
+                "Falta FIREBASE_CREDENTIALS"
+            )
+
+        cred_dict = json.loads(firebase_json)
+
         cred = credentials.Certificate(
-            "firebase_key.json"
+            cred_dict
         )
 
         firebase_admin.initialize_app(cred)
 
+
+# =====================================
+# DB
+# =====================================
 
 def db():
     iniciar_firebase()
